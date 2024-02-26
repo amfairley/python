1b98dfbe193a0ea87ba0026cbfd3729e2a01b3e8
amfairley <adamfairley1990@gmail.com>
2024-02-26 09:48:41 +0000

    refactor: add main and __name__ == __main__

--- project_euler/euler_1.py
+++ project_euler/euler_1.py
@@ -1,20 +1,23 @@
 # Find the sum of the multiples of 3 or 5 below 1000
+def main():
 
-# Make an empty list
-numbers = []
+    # Make an empty list
+    numbers = []
 
-# Add all multiples of 3 to the list
-for i in range(0,1000,3):
-    numbers.append(i)
-
-# Add all multiples of 5 that are already not in the list
-for i in range(0,1000,5):
-    if i not in numbers:
+    # Add all multiples of 3 to the list
+    for i in range(0,1000,3):
         numbers.append(i)
-    else:
-        continue
 
-# Sum all the numbers
-sum = sum(numbers)
+    # Add all multiples of 5 that are already not in the list
+    for i in range(0,1000,5):
+        if i not in numbers:
+            numbers.append(i)
+        else:
+            continue
 
-print(sum)
+    # Sum all the numbers
+    answer = sum(numbers)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
