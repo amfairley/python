2d6f8f079b3e3a6773b6889bd08219eee0d21f5e
amfairley <adamfairley1990@gmail.com>
2024-02-26 09:59:40 +0000

    refactor: add docstrings to functions

--- project_euler/euler_functions.py
+++ project_euler/euler_functions.py
@@ -1,5 +1,5 @@
-# Checking if a value is prime:
 def is_prime(x):
+    """A simple function that determines whether an input integer is prime or not"""
     factor_list = []
     for i in range (1,x):
         if x % i == 0:
@@ -8,3 +8,22 @@ def is_prime(x):
         return True
     else:
         return False
+    
+
+
+def divisible(n,x,y):
+    """A simple function that takes an integer input n, and outputs if it is divisible by a range of x-y"""
+    # Range of items to divide by
+    divisors = list(range(x,y+1))
+    # Double check of divisors
+    print(divisors)
+    numbers = []
+    for i in divisors:
+        if n % i == 0:
+            numbers.append(i)
+    # If divisible by all the numbers return true
+    if len(numbers) == len(divisors):
+        return True
+    else:
+        return False
+
