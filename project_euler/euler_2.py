774a0a4e793e96e2d4d480c918e8dc9f223383e3
amfairley <adamfairley1990@gmail.com>
2024-02-26 09:51:13 +0000

    refactor: add main and __name__ == __main__

--- project_euler/euler_2.py
+++ project_euler/euler_2.py
@@ -1,18 +1,29 @@
 # Sum of even values in Fibonacci Sequence up to an including 4 million
-# Fibonacci Sequence
-fibonacci_sequence = [1, 2]
-while len(fibonacci_sequence) < 4000001:
-    new_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-    fibonacci_sequence.append(new_number)
+import numpy as np
 
-# Get the even numbers
-even_numbers = []
-for i in fibonacci_sequence:
-    if i % 2 == 0:
-        even_numbers.append(i)
+def main():
+    # Fibonacci Sequence
+    fibonacci_sequence = np.array([1,2])
+    while fibonacci_sequence.size < 4000001:
+        new_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence = np.append(fibonacci_sequence, new_number)
 
-# Sum the even numbers
-sum = sum(even_numbers)
+    # Get the even numbers
+    evens = np.array([])
+    for i in fibonacci_sequence:
+            if i % 2 == 0:
+                evens = np.append(evens, i)
 
-print(sum)
+    # Sum the even numbers
+
+    sum = np.sum(evens)
+    print(sum)
+
+if __name__ == "__main__":
+     main()
+
+
+
+
+
 
