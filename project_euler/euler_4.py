8bae1ab7663ddead21d15c51bda07d48382b189e
amfairley <adamfairley1990@gmail.com>
2024-02-26 10:00:43 +0000

    refactor: add main and __name__ == __main__

--- project_euler/euler_4.py
+++ project_euler/euler_4.py
@@ -1,19 +1,22 @@
 # Find the largest palindrome made from the product of two 3-digit numbers
-# Palindromes"
-palindromes = []
-for i in range(0,999):
-    for j in range(0,999):
-        product = i * j
-        # Convert the answer to a string
-        product = str(product)
-        if product not in palindromes:
-            if len(product) < 6:
-                continue
-            # elif ((len(product) == 2) or (len(product) == 3)) & (product[0] == product [-1]):
-            #     palindromes.append(product)
-            # elif (len(product) == 4 or len(product) == 5) & (product[0] == product [-1]) & (product[1] == product [-2]):
-            #     palindromes.append(product)
-            elif (len(product) == 6) & (product[0] == product [-1]) & (product[1] == product [-2]) & (product[2] == product[3]):
-                palindromes.append(int(product))
+def main():
+    palindromes = []
+    for i in range(0,999):
+        for j in range(0,999):
+            product = i * j
+            # Convert the answer to a string
+            product = str(product)
+            if product not in palindromes:
+                if len(product) < 6:
+                    continue
+                # elif ((len(product) == 2) or (len(product) == 3)) & (product[0] == product [-1]):
+                #     palindromes.append(product)
+                # elif (len(product) == 4 or len(product) == 5) & (product[0] == product [-1]) & (product[1] == product [-2]):
+                #     palindromes.append(product)
+                elif (len(product) == 6) & (product[0] == product [-1]) & (product[1] == product [-2]) & (product[2] == product[3]):
+                    palindromes.append(int(product))
 
-print(max(palindromes))
+    print(max(palindromes))
+
+if __name__ == "__main__":
+    main()
